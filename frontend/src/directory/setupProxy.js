904342365e50791adc7e99b5b902c59a44c607c3
Mmoso <moosemodise4@gmail.com>
2023-07-21 11:04:37 +0200

    heroku updates

--- frontend/src/directory/setupProxy.js
+++ frontend/src/directory/setupProxy.js
@@ -2,7 +2,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/" , "/otherApi"], {
-      target: "http://localhost:8080",
+      // target: "http://localhost:8080",
+      target: "https://evening-lake-82112-ba62a6e69b2a.herokuapp.com/",
     })
   );
 };
+// https://evening-lake-82112-ba62a6e69b2a.herokuapp.com/
