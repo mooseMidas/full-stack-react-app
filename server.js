8cf7de8e2b2f1157832b3336f083f7992dfb91f1
Mmoso <moosemodise4@gmail.com>
2023-07-21 17:23:11 +0200

    test two

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 
 const app = require("./app");
 const port = process.env.PORT || 8080;
+const express = require("express");
+const path = require("path");
 
 app.listen(port, () => console.log("Server started on port 8080"));
 
-
-
+app.use(express.static(path.join(__dirname, "frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+});
