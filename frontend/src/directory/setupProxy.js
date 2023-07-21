ba807135840c31d633ed4403a443f164a294dfd1
Mmoso <moosemodise4@gmail.com>
2023-07-21 10:41:38 +0200

    build file

--- frontend/src/directory/setupProxy.js
+++ frontend/src/directory/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(["/api", , "/otherApi"], {
+    createProxyMiddleware(["/" , "/otherApi"], {
       target: "http://localhost:8080",
     })
   );
